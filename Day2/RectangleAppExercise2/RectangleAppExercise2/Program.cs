b64584cafb661a595a4f79fb9e24e181c8e53613
David Upsdale <david.upsdale@thetrainline.com>
2017-01-17 10:39:51 +0000

    Day 2 start

---
+++ Day2/RectangleAppExercise2/RectangleAppExercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleAppExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            rect.Width = 80;
+
+            Console.WriteLine("Area = {0}", rect.Area());
+            Console.WriteLine("Perimeter = {0}", rect.Perimeter());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day2/RectangleAppExercise2/RectangleAppExercise2/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleAppExercise2
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public double Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        /// <summary>
+        /// Return the area of the rectangle
+        /// </summary>
+        /// <returns></returns>
+        public double Area()
+        {
+            return length*width;
+        }
+
+        /// <summary>
+        /// Return the perimeter of the rectangle
+        /// </summary>
+        /// <returns></returns>
+        public double Perimeter()
+        {
+            return 2*(length + width);
+        }
+    }
+}
