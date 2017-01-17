35e9ea5a0af226e619a17c1dae05e2c6e44357b6
David Upsdale <david.upsdale@thetrainline.com>
2017-01-17 13:11:35 +0000

    SwapApp finish

--- Day2/SwapApp/SwapApp/Program.cs
+++ Day2/SwapApp/SwapApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +9,29 @@ namespace SwapApp
 {
     class Program
     {
-        static void SwapByVal(ref int a, ref int b)
+        static void SwapByVal(int a, int b)
         {
             int temp = a;
             a = b;
             b = temp;
 
-            Console.WriteLine("Inside a = {0}, b = {1}", a, b);
+            Console.WriteLine("Inside SwapByVal a = {0}, b = {1}", a, b);
+        }
+
+        static void SwapByRef(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("Inside SwapByRef a = {0}, b = {1}", a, b);
+        }
+
+        static void Add(int a, int b, out int c)
+        {
+            c = a + b;
+
+            c += 100;
         }
         static void Main(string[] args)
         {
@@ -22,9 +39,29 @@ namespace SwapApp
             int b = 2;
             Console.WriteLine("Outside a = {0}, b = {1}", a, b);
 
-            SwapByVal(ref a, ref b);
             Console.WriteLine();
+
+            SwapByVal(a, b);
             Console.WriteLine("Outside a = {0}, b = {1}", a, b);
+
+            Console.WriteLine();
+
+            SwapByRef(ref a, ref b);
+            Console.WriteLine("Outside a = {0}, b = {1}", a, b);
+
+            Console.WriteLine();
+
+            int c = 0;
+
+            Console.WriteLine("c is {0}", c);
+
+            Add(a, b, out c);
+
+            Console.WriteLine();
+
+            Console.WriteLine("c is {0}", c);
+
+
 
             Console.ReadKey();
 
